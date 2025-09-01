5d7fa801426b75127047a7a07c4b691e87075bdb
titilolami25 <titilolami25@gmail.com>
2025-09-01 14:42:04 +0100

    import product repetiton removed in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Product from "./pages/AllProducts";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
